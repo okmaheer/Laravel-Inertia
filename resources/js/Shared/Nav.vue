0f98d4531be858121309ceecb2c155bb822b746d
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-05-22 22:43:18 +0500

    Update Nav.vue

--- resources/js/Shared/Nav.vue
+++ resources/js/Shared/Nav.vue
@@ -1,14 +1,17 @@
 <template>
-    <nav>
-        <ul>
+    <nav class="mt-6">
+        <ul class="list-disc">
             <li>
-                <Link href="/">Home</Link>
+                <Link href="/" class="text-blue-500 hover:underline">Home</Link>
             </li>
             <li>
-                <Link href="/users">Users</Link>
+                <Link href="/users" class="text-blue-500 hover:underline">Users</Link>
             </li>
             <li>
-                <Link href="/settings">Settings</Link>
+                <Link href="/settings" class="text-blue-500 hover:underline">Settings</Link>
+            </li>
+            <li>
+                <Link href="/logout" :data="{ name:'Maheer'}" class="text-blue-500 hover:underline" method="POST" as="button">LogOut</Link>
             </li>
 
 
