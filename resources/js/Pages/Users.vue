273c4b2d00fb20319b3e54fc7702561d0e7a4afc
Maheer <86462580+okmaheer@users.noreply.github.com>
2023-05-22 22:43:16 +0500

    Update Users.vue

--- resources/js/Pages/Users.vue
+++ resources/js/Pages/Users.vue
@@ -1,11 +1,18 @@
 <template>
     <h1>Users</h1>
     <Nav/>
-  
+   <div style="margin-top: 800px;" >
+<p> 
+    The current ime is {{time }}
+</p>
+   </div>
 </template>
 <script>
 import Nav from '../Shared/Nav.vue'
 export default {
     components: { Nav },
+    props:{
+        time:String
+    }
 }
 </script>
